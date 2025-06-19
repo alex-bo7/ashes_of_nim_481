6ed727a23c2edc061459fe4bc3a03ec24a6dbdbc
alex-bo7 <anino882@gmail.com>
2025-06-19 07:39:10 -0700

    Setup game settings

--- MainMenu/main_menu.gd
+++ MainMenu/main_menu.gd
@@ -6,7 +6,7 @@ func _on_play_btn_pressed() -> void:
 
 
 func _on_settings_btn_pressed() -> void:
-	print('Settings')
+	get_tree().change_scene_to_file("res://Settings/settings.tscn")
 
 func _on_quit_btn_pressed() -> void:
 	get_tree().quit()

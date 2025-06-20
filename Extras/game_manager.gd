085557a8a97560ed2a48071fdcfac12e4be14002
alex-bo7 <anino882@gmail.com>
2025-06-20 05:59:57 -0700

    Selecting match sticks in-game

--- Extras/game_manager.gd
+++ Extras/game_manager.gd
@@ -4,3 +4,13 @@ extends Node
 # handle match selection
 # make code work with project 2 code
 # handle player turn?
+enum turn {PLAYER, CPU}
+var current_turn: turn = turn.PLAYER # player plays first
+
+
+var matches_arr: Array = GameSettings.matches_arr
+var depth_lim: int = GameSettings.depth_lim
+# for algorithm call is_algo
+
+var current_selected_row: int = -1
+var selected_matches: Array = []
--- MatchStick/match_stick.gd
+++ MatchStick/match_stick.gd
@@ -4,9 +4,28 @@ extends RigidBody3D
 @export var match_stick_black: Node3D
 @export var match_stick_red: Node3D
 
+# click select match
+# click again deselect match
 
-func _input(event: InputEvent) -> void:
-	if event is InputEventMouseButton and event.is_pressed():
-		#match_stick_red.visible = true
-		#match_stick_black.visible = false
-		print(row_idx)
+func select_match() -> void:
+	match_stick_red.visible = true
+	match_stick_black.visible = false
+	GameManager.selected_matches.append(self)
+
+
+func deselect_match() -> void:
+	match_stick_red.visible = false
+	match_stick_black.visible = true
+	GameManager.selected_matches.erase(self)
+
+
+func on_click() -> void:
+	if (self in GameManager.selected_matches):
+		deselect_match()
+	elif GameManager.current_selected_row == -1 or GameManager.selected_matches.size() == 0:
+		GameManager.current_selected_row = row_idx
+		select_match()
+	elif GameManager.current_selected_row == row_idx:
+		select_match()
+	else:
+		print("Must select matches in current row: ", row_idx)
